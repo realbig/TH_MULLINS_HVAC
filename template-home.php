9f204e1b8fd352ffbeca4c1733c492c7281bff1c
Eric Defore <eric.defore@gmail.com>
2015-11-04 11:14:16 -0500

    Added support for the Angie's List Badge. We need it from the client. #7

--- template-home.php
+++ template-home.php
@@ -137,25 +137,37 @@ the_post();
 
             </div>
 
-            <div id="testimonial">
+            <div class="row">
 
-                <div class="row">
-                    <div class="small-12 columns text-center">
-                        <span class="fa fa-spinner fa-spin testimonial-loading"></span>
+                <div class="small-12 medium-4 columns text-center">
+                    <div class="row">
+                        <div class="small-12 columns">
+                            <img src="#" />
+                        </div>
                     </div>
                 </div>
-                <div class="testimonial-content">
+
+                <div id="testimonial" class="small-12 medium-8 columns">
+
                     <div class="row">
-                        <div class="small-12 columns aligncenter">
-                            <blockquote class="testimonial-copy"></blockquote>
+                        <div class="small-12 columns text-center">
+                            <span class="fa fa-spinner fa-spin testimonial-loading"></span>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="small-12 columns text-right">
-                            <h4></h4>
-                            <a href="#" data-reveal-id="testimonial-modal">Leave Your Own Testimonial</a>
+                    <div class="testimonial-content">
+                        <div class="row">
+                            <div class="small-12 columns aligncenter">
+                                <blockquote class="testimonial-copy"></blockquote>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="small-12 columns text-right">
+                                <h4></h4>
+                                <a href="#" data-reveal-id="testimonial-modal">Leave Your Own Testimonial</a>
+                            </div>
                         </div>
                     </div>
+
                 </div>
 
             </div>
